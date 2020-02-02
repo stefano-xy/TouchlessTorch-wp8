c7ef9677fd2a07c453065d0244c8972e4606c354
Stefano Pettini <stefano@pettini.eu>
2020-02-02 18:05:27 +0100

    Sources imported from the original project

---
+++ TouchlessTorch/Controller.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Phone.Media.Capture;
+
+namespace TouchlessTorch
+{
+    public class Controller
+    {
+        public enum StatusEnum
+        {
+            Uninitialized,
+            Ready,
+            CannotFindBackCamera,
+            CannotFindValidResolutionForCamera,
+            CannotAccessCameraDevice,
+            CannotFindTorchOnDevice
+        }
+
+        const CameraSensorLocation _location = CameraSensorLocation.Back;
+
+        AudioVideoCaptureDevice _device = null;
+
+        StatusEnum _status = StatusEnum.Uninitialized;
+
+        public Controller()
+        {
+
+        }
+
+        public async Task InitAsync()
+        {
+            Debug.Assert(_status == StatusEnum.Uninitialized);
+
+            if (AudioVideoCaptureDevice.AvailableSensorLocations.Contains(_location))
+            {
+                var resolutions = AudioVideoCaptureDevice.GetAvailableCaptureResolutions(_location);
+
+                if (resolutions.Count > 0)
+                {
+                    var resolution = resolutions.First();
+
+                    _device = await AudioVideoCaptureDevice.OpenForVideoOnlyAsync(_location, resolution);
+
+                    if ((object)_device != null)
+                    {
+                        var values = AudioVideoCaptureDevice.GetSupportedPropertyValues(_location, KnownCameraAudioVideoProperties.VideoTorchMode);
+
+                        if (values.Contains((UInt32)VideoTorchMode.On))
+                        {
+                            // Done, it looks like we have it!
+
+                            _status = StatusEnum.Ready;
+                        }
+                        else
+                        {
+                            _status = StatusEnum.CannotFindTorchOnDevice;
+                            _device.Dispose();
+                            _device = null;
+                        }
+                    }
+                    else
+                    {
+                        _status = StatusEnum.CannotAccessCameraDevice;
+                    }
+                }
+                else
+                {
+                    _status = StatusEnum.CannotFindValidResolutionForCamera;
+                }
+            }
+            else
+            {
+                _status = StatusEnum.CannotFindBackCamera;
+            }
+        }
+
+        public void Term()
+        {
+            if (_status == StatusEnum.Ready)
+            {
+                _device.Dispose();
+                _device = null;
+            }
+
+            _status = StatusEnum.Uninitialized;
+        }
+
+        public bool Ready
+        {
+            get { return _status == StatusEnum.Ready; }
+        }
+        
+        public StatusEnum Status
+        {
+            get { return _status; }
+        }
+
+        public bool Enabled
+        {
+            set
+            {
+                Debug.Assert(_status == StatusEnum.Ready);
+                Debug.Assert((object)_device != null);
+                
+                if (value)
+                {
+                    _device.SetProperty(KnownCameraAudioVideoProperties.VideoTorchMode, VideoTorchMode.On);
+                    _device.SetProperty(KnownCameraAudioVideoProperties.VideoTorchPower,
+                        AudioVideoCaptureDevice.GetSupportedPropertyRange(_location, KnownCameraAudioVideoProperties.VideoTorchPower).Max);
+                }
+                else
+                {
+                    _device.SetProperty(KnownCameraAudioVideoProperties.VideoTorchMode, VideoTorchMode.Off);
+                    _device.SetProperty(KnownCameraAudioVideoProperties.VideoTorchPower,
+                        AudioVideoCaptureDevice.GetSupportedPropertyRange(_location, KnownCameraAudioVideoProperties.VideoTorchPower).Min);
+                }
+            }
+        }
+    }
+}
---
+++ TouchlessTorch/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace TouchlessTorch
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        readonly Controller _controller = new Controller();
+        readonly DispatcherTimer _timer = new DispatcherTimer();
+        readonly Brush _onBrush = (Brush)App.Current.Resources["PhoneForegroundBrush"];
+        readonly Brush _offBrush = (Brush)App.Current.Resources["PhoneInactiveBrush"];
+        readonly Brush _emergencyBrush = new SolidColorBrush(Colors.Red);
+        
+        public MainPage()
+        {
+            InitializeComponent();           
+
+            _timer.Interval = new TimeSpan(0, 0, 2);
+            _timer.Tick += TimerTick;
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_controller.Ready)
+                _controller.Enabled = true;
+
+            UpdateClock();
+            UpdateBattery();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Debug.WriteLine(this.GetHashCode() + ", OnNavigatedTo " + e.NavigationMode.ToString());
+
+            base.OnNavigatedTo(e);
+
+            if (e.NavigationMode != NavigationMode.Reset)
+            {
+                UpdateClock();
+                UpdateBattery();
+
+                if (! _controller.Ready)
+                    await _controller.InitAsync();
+
+                if (_controller.Ready)
+                    _controller.Enabled = true;
+
+                _timer.Start();
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            Debug.WriteLine(this.GetHashCode() + ", OnNavigatedFrom " + e.NavigationMode.ToString());
+
+            _timer.Stop();
+
+            if (_controller.Ready)
+                _controller.Enabled = false;
+
+            _controller.Term();
+
+            base.OnNavigatingFrom(e);
+        }
+
+        private void UpdateClock()
+        {
+            TimeDisplay.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void UpdateBattery()
+        {
+            int charge = Windows.Phone.Devices.Power.Battery.GetDefault().RemainingChargePercent;
+            bool charging = Microsoft.Phone.Info.DeviceStatus.PowerSource == Microsoft.Phone.Info.PowerSource.External;
+
+            BatteryDisplay.Text = charge.ToString();
+
+            p0.Fill = (!charging && charge < 20) ? _emergencyBrush : _offBrush;
+            p1.Fill = (!charging && charge >= 20 && charge < 40) ? _onBrush : _offBrush;
+            p2.Fill = (!charging && charge >= 40 && charge < 80) ? _onBrush : _offBrush;
+            p3.Fill = (!charging && charge >= 80) ? _onBrush : _offBrush;
+            pc.Fill = charging ? _onBrush : _offBrush;
+        }
+    }
+}
